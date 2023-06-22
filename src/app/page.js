9ece741117d2f5e2c20bd1fe272a1556152a0558
Agustin Somoza Polchi <aspolchi@gmail.com>
2023-06-21 21:26:01 -0300

    starting

---
+++ src/app/page.js
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import Presentation from "./components/presentation/Presentation";
+
+export default function Home() {
+  return (
+    <main>
+    <Presentation/>
+    </main>
+  
+  );
+}
