529924e173910477be08e21f15bbc1e6100d3541
Agustin Somoza Polchi <aspolchi@gmail.com>
2023-07-17 18:05:00 -0300

    em no

--- src/app/components/weAreProud/WeAreProud.jsx
+++ src/app/components/weAreProud/WeAreProud.jsx
@@ -1,18 +1,57 @@
-'use client'
-import React from 'react'
-import './weAreProud.scss'
-import {motion} from 'framer-motion'
+"use client";
+import React from "react";
+import "./weAreProud.scss";
+import { motion } from "framer-motion";
+import logoArtbag from "../../images/logos/LOGOARTBAG.png";
+import bancoFormosa from "../../images/logos/LOGOBANCOFORMOSA.png";
+import wyndham from "../../images/logos/LOGOWYNDHAM.png";
+import rockCycle from "../../images/logos/LOGOROCKCYCLE.png";
+import piba from "../../images/logos/LOGOPIBA.png";
+import meta from "../../images/logos/LOGOMETA.png";
+import diamondsFilm from "../../images/logos/LOGODIAMONDFILMS.png";
+import dannyOcean from "../../images/logos/LOGODANNYOCEAN.png";
+import bodegaAraujo from "../../images/logos/LOGOBODEGAARAUJO.png";
+import Image from "next/image";
 
 const WeAreProud = () => {
-    const frase = "we're proud To work with"
+  // make an array with the logos imported
+  const logos = [
+    { clase: "logoArtbag", logo: logoArtbag },
+    { clase: "bancoFormosa", logo: bancoFormosa },
+    { clase: "wyndham", logo: wyndham },
+    { clase: "rockCycle", logo: rockCycle },
+    { clase: "piba", logo: piba },
+    { clase: "meta", logo: meta },
+    { clase: "diamondsFilm", logo: diamondsFilm },
+    { clase: "dannyOcean", logo: dannyOcean },
+    { clase: "bodegaAraujo", logo: bodegaAraujo },
+  ];
+  const frase = "we're proud To work with";
   return (
-    <motion.section whileInView={{opacity:1}}  className="weAreProud">
-        <div className="weAreProud__container">
-            <h2>{frase}</h2>
-            
+    <motion.section className="weAreProud">
+      <div className="weAreProud__container">
+        <div className="weAreProud__container__title">
+          <h2>{frase}</h2>
         </div>
+        <div className="weAreProud__container__content">
+          <div className="weAreProud__container__content__container">
+            {logos.map((item, i) => (
+              <div
+                className="weAreProud__container__content__container__logo"
+                key={i}
+              >
+                <Image
+                  src={item.logo}
+                  alt={item.clase}
+                  className={`weAreProud__container__content__container__logo__marca ${item.clase}`}
+                />
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
     </motion.section>
-  )
-}
+  );
+};
 
-export default WeAreProud
+export default WeAreProud;
