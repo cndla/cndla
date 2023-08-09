79b16a5f7c81ce5fa161999dd9767665c7bee3f0
Agustin Somoza Polchi <aspolchi@gmail.com>
2023-08-09 19:41:59 -0300

    yep

--- src/app/components/lab/Lab.jsx
+++ src/app/components/lab/Lab.jsx
@@ -1,36 +1,32 @@
-'use client'
+"use client";
 import React from "react";
 import "./lab.scss";
-import { motion } from 'framer-motion'
+import { motion } from "framer-motion";
 
 const Lab = () => {
+  const text1 = "an innovation lab that builds ideas and projects"
+  const text2 = "that reshape culture and entertainment."
+  const title = '<span>CNDLA</span><span>\</span> <span>LAB</span>'
   return (
     <section className="lab">
       <div className="lab__container">
-        <h2 className="lab__container__title">
-          <span>CNDLA</span>
-          <span>\</span>
-          <span>LAB</span>
-        </h2>
-        <div className="lab__container__subtitle">
-          <div className="lab__container__subtitle__top">
-            <motion.div
-              initial={{ "width": 0 }}
-              whileInView={{ "width": "33%" }}
-              transition={{ duration: 0.5 }}
-              className="lab__container__subtitle__top__hr"></motion.div>
-            an innovation lab that builds ideas and projects
-          </div>
-          <div className="lab__container__subtitle__bottom">
-            that reshape culture and entertainment.
-            <motion.div
-              initial={{ "width": 0 }}
-              whileInView={{ "width": "35%" }}
-              transition={{ duration: 0.5 }}
-              className="lab__container__subtitle__bottom__hr"></motion.div>
-          </div>
-        </div>
+      <motion.div 
+      //i want to animate a line from right to left starting with 0 width 
+      initial={{width:0, transformOrigin:'left'}}
+      whileInView={{width:'100%'}}
+      transition={{duration:1}}
+       className="lab__container__hr top"></motion.div>
+      <div className="lab__container__content">
+        <h2 className="lab__container__content__title">cndla\lab</h2>
+        <p className="lab__container__content__subtitle">{text1}</p>
+        <p className="lab__container__content__subtitle">{text2}</p>
       </div>
+      <motion.div 
+      //i want to animate a line from right to left starting with 0 width 
+      initial={{width:0, transformOrigin:'left'}}
+      whileInView={{width:'100%'}}
+      transition={{duration:1, delay:1}}
+       className="lab__container__hr bot"></motion.div>      </div>
     </section>
   );
 };
