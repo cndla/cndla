206481b99df7e512eb6ac7ad79fc6184e6cec9ba
Agustin Somoza Polchi <aspolchi@gmail.com>
2023-12-23 00:08:03 -0300

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,9 +1,8 @@
-'use client'
+"use client";
 import Header from "./components/header/Header";
 import "./globals.css";
 import { Roboto } from "next/font/google";
 import { ReactLenis, useLenis } from "@studio-freight/react-lenis";
-
 
 const roboto = Roboto({ subsets: ["latin"], weight: "400" });
 
@@ -17,11 +16,10 @@ export default function RootLayout({ children }) {
     // called every scroll
   });
   return (
-    <html lang="en">
-      <ReactLenis root>
+    <ReactLenis root>
+      <html lang="en">
         <body>{children}</body>
-      </ReactLenis>
-    </html>
+      </html>
+    </ReactLenis>
   );
 }
-
