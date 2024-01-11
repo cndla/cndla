094c0c36b5faf4435841a21044a9d57c8df8dcea
Agustin Somoza Polchi <aspolchi@gmail.com>
2024-01-11 11:48:09 -0300

    ahi ?

--- src/app/components/onFire/OnFire.js
+++ src/app/components/onFire/OnFire.js
@@ -6,21 +6,7 @@ import './onFire.scss'
 const OnFire = () => {
   return (
     <section className="onFire">
-      <div className="onFire__container">
-        <h2 className="onFire__container__title">on fire sessions</h2>
-        <div className="onFire__container__content">
-          <p>
-            we are surrounded by extremely talented people. trends hunters and
-            fascinating
-          </p>
-          <p>
-            people who are in a constant persuit of learning, evolving and
-            growing, and aeger
-          </p>
-          <p>to share everything the know as well</p>
-        </div>
-        <Image className="onFire__container__image" src={inspire} alt="inspire" width={"auto"} />
-      </div>
+
     </section>
   );
 };
