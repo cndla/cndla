dfabe330075e7e9b9c47e7258aedfd5544c6e587
Agustin Somoza Polchi <aspolchi@gmail.com>
2024-03-20 12:17:51 -0300

    Update Lab.jsx

--- src/app/components/lab/Lab.jsx
+++ src/app/components/lab/Lab.jsx
@@ -5,7 +5,7 @@ import LabCreating from "./LabCreating";
 import { AnimatePresence } from "framer-motion";
 
 export const Logo = ({ text, borderColor, extraClass }) => (
-  <div className={`${extraClass} lg:border-4  border-2 ${borderColor ? borderColor : "border-black"} flex  lg:p-2 p-3  rounded-full lg:max-w-64 max-w-40 font-bold justify-center lg:text-4xl text-xl text-nowrap w-12/12 `}>
+  <div className={`${extraClass} lg:border-4  border-2 ${borderColor ? borderColor : "border-black"} flex  lg:p-2 p-3  rounded-full lg:max-w-64 max-w-40 font-bold justify-center lg:text-4xl text-lg text-nowrap `}>
     <span className="px-3 font-normal text-nowrap ">{text}</span>
   </div>
 );
